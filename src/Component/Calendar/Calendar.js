249266f0d20dab4646e6c2515694b5e63623dc71
Saswati Priyadarshini <spriyadarshini@crosslight.io>
2019-10-31 12:43:38 +0530

    get current and selected date

--- src/Component/Calendar/Calendar.js
+++ src/Component/Calendar/Calendar.js
@@ -1,22 +1,39 @@
-import React, { Component } from 'react';
-import { Calendar } from 'antd';
+import React, { Component, Fragment } from 'react';
+import { Calendar, Alert } from 'antd';
+import moment from 'moment';
 import "antd/dist/antd.css";
 import './style.css';
 export default class CalendarComp extends Component{
 	constructor(props){
 		super(props);
 		this.state = {
-
+			value: moment(),
+			selectedValue: moment(),
 		}
 	}
 	onPanelChange(value, mode) {
 		console.log(value, mode);
 	}
+
+	onSelect = value => {
+		this.setState({
+			value,
+			selectedValue: value,
+		});
+	};
+
 	render(){
+		const { value, selectedValue } = this.state;
 		return(
-			<div style={{ width: 300, border: '1px solid #d9d9d9', borderRadius: 4 }} className='calender__container'>
-				<Calendar fullscreen={false}  />
-			</div>
+			<Fragment>
+				<Alert
+					message={`You selected date: ${selectedValue && selectedValue.format('YYYY-MM-DD')}`}
+				/>
+				<div style={{ width: 300, border: '1px solid #d9d9d9', borderRadius: 4 }} className='calender__container'>
+					<Calendar value={value} onSelect={this.onSelect} fullscreen={false}  />
+				</div>
+			</Fragment>
+
 		)
 	}
 }
